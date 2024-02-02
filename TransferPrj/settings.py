0d1c314825c325366474a4b877f3770938fdf206
succy2024 <157708869+succy2024@users.noreply.github.com>
2024-02-02 02:59:54 -0800

    made change to forms and made texts bolder

--- TransferPrj/settings.py
+++ TransferPrj/settings.py
@@ -95,24 +95,24 @@ WSGI_APPLICATION = 'TransferPrj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'mysql.connector.django',
-#         'NAME': 'succylog_newDb',
-#         'USER': 'succylog_admin',
-#         'PASSWORD': 'succyloglobalfx2024',
-#         'HOST': '50.87.151.187',  # Set to your MySQL server's hostname or IP address
-#         'PORT': '3306',       # Set to your MySQL server's port (default is 3306)
-#         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" ,"use_pure": True},
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'succylog_newDb',
+        'USER': 'succylog_admin',
+        'PASSWORD': 'succyloglobalfx2024',
+        'HOST': '50.87.151.187',  # Set to your MySQL server's hostname or IP address
+        'PORT': '3306',       # Set to your MySQL server's port (default is 3306)
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" ,"use_pure": True},
+    }
+}
 
 
 
